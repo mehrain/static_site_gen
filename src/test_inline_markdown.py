07fab5bf9a896cf0afe1a9e1cc24708e39bdc6a3
mehrain <marijnrovers@live.nl>
2024-03-10 21:43:58 +0100

    addition and filename changes

--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -1,6 +1,6 @@
 import unittest
 from textnode import *
-from node_delimiter import *
+from inline_markdown import *
 
 class TestSplitNodesDelimiter(unittest.TestCase):
     def test_split_text_nodes(self):
